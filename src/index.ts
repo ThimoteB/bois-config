50ff2e1ed65ab00160be64ad4ee418938537dbf1
Raku <92573663+ThimoteB@users.noreply.github.com>
2024-06-07 16:08:02 +0200

    Feature/add docs (#1)
    * test
    
    * fix: test
    
    * fix: add

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+const sum = (a: number, b: number): number => a + b;
+
+const divide = (a: number, b: number): number => a / b;
+
+const multiply = (a: number, b: number): number => a * b;
+
+const printSum = (a: number, b: number): void => {
+  debugger;
+  console.log(`Sum of ${a} and ${b} is ${sum(a, b)}`);
+};
+
+export { sum, divide, multiply, printSum };
